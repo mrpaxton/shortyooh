04ff2d526436d85f7551482e4fc72de24577dbfe
Sarn Wattanasri <mr.paxton@gmail.com>
2017-04-18 22:10:29 -0700

    Simple implementation of shortcode generator

--- src/shorterner/models.py
+++ src/shorterner/models.py
@@ -1,10 +1,20 @@
 from django.db import models
+import random
+import string
+
+
+def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 class ShortURL(models.Model):
     url = models.CharField(max_length=220, )
     shortcode = models.CharField(max_length=15, unique=True)
     updated = models.DateTimeField(auto_now=True) #when model saved
     timestamp = models.DateTimeField(auto_now_add=True) #when model screated
+
+    def save(self, *args, **kwargs):
+        self.shortcode = code_generator()
+        super(ShortURL, self).save(*args, **kwargs)
 
     def __str__(self):
         return str(self.url)
