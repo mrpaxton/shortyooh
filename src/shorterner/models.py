b10d3cc423f29e4fdea73d61e37e2495b831f32f
Sarn Wattanasri <mr.paxton@gmail.com>
2017-04-19 01:29:46 -0700

    Added support for refresh_shortcodes

--- src/shorterner/models.py
+++ src/shorterner/models.py
@@ -1,5 +1,22 @@
 from django.db import models
-from utils import create_shortcode
+from .utils import create_shortcode
+
+
+class ShortURLManager(models.Manager):
+    def all(self, *args, **kwargs):
+        qs_main = super(ShortURLManager, self).all(*args, **kwargs)
+        qs = qs_main.filter(active=False)
+        return qs
+
+    def refresh_shortcodes(self):
+        qs = ShortURL.objects.filter(id__gte=1)
+        new_codes = 0
+        for q in qs:
+            q.shortcode = create_shortcode(q)
+            print(q.shortcode)
+            q.save()
+            new_codes += 1
+        return "New codes refreshed: {num}".format(num=new_codes)
 
 
 class ShortURL(models.Model):
@@ -7,6 +24,9 @@ class ShortURL(models.Model):
     shortcode = models.CharField(max_length=15, unique=True, blank=True)
     updated = models.DateTimeField(auto_now=True) #when model saved
     timestamp = models.DateTimeField(auto_now_add=True) #when model screated
+    active = models.BooleanField(default=True)
+
+    objects = ShortURLManager()
 
     def save(self, *args, **kwargs):
         if not self.shortcode or self.shortcode == "":
