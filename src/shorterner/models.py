ecca0c9e314f1b614f179c268b77de411960dd9d
Sarn Wattanasri <mr.paxton@gmail.com>
2017-04-18 23:19:16 -0700

    Save shortcode only when it is empty or None; Refactor creating shortcode

--- src/shorterner/models.py
+++ src/shorterner/models.py
@@ -1,19 +1,16 @@
 from django.db import models
-import random
-import string
+from utils import create_shortcode
 
-
-def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
 
 class ShortURL(models.Model):
     url = models.CharField(max_length=220, )
-    shortcode = models.CharField(max_length=15, unique=True)
+    shortcode = models.CharField(max_length=15, unique=True, blank=True)
     updated = models.DateTimeField(auto_now=True) #when model saved
     timestamp = models.DateTimeField(auto_now_add=True) #when model screated
 
     def save(self, *args, **kwargs):
-        self.shortcode = code_generator()
+        if not self.shortcode or self.shortcode == "":
+            self.shortcode = create_shortcode(self)
         super(ShortURL, self).save(*args, **kwargs)
 
     def __str__(self):
