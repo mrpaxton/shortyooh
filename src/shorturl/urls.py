e50cf2546086b910843179cf12696e67bc1167e2
Sarn Wattanasri <mr.paxton@gmail.com>
2017-04-19 17:29:55 -0700

    URL parameter from the router using regular expression matching for shortcode

--- src/shorturl/urls.py
+++ src/shorturl/urls.py
@@ -19,8 +19,7 @@ from django.contrib import admin
 from shorterner.views import shorturl_redirect_view, ShorturlCBView
 
 urlpatterns = [
-    #url(r'^admin/', admin.site.urls),
-    url(r'^new-admin/', admin.site.urls),
-    url(r'^view-1/$', shorturl_redirect_view),
-    url(r'^view-2/$', ShorturlCBView.as_view()),
+    url(r'^suadmin/', admin.site.urls),
+    url(r'^fbv/(?P<shortcode>[\w-]+)/$', shorturl_redirect_view),
+    url(r'^cbv/(?P<shortcode>[\w-]+)/$', ShorturlCBView.as_view()),
 ]
